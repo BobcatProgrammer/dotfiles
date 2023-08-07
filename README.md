1cff0c9d3002d97546ccdf2aade9e11865fc50a8
Simon Trockel <mail@simon-trockel.de>
2023-08-07 19:47:48 +0000

    Add bootstrap & change install script to yadm clone

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # dotfiles
+
 My dotfiles for MacOS &amp; Debian
+
+## Use
+
+The dotfiles are managed using yadm. To use them you first need to install it via the package manager (Homebrew or apt).
+
+## Mac OS
+
+### Install Homebrew
+
+`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"`
+
+### Install yadm (Mac OS)
+
+`brew install yadm`
+
+## Debian
+
+### Install yadm (Debian)
+
+`sudo apt yadm`
+
+## Clone dotfiles
+
+'''sh
+yadm clone https://github.com/BobcatProgrammer/dotfiles
+'''
--- install.sh
+++ install.sh
@@ -6,49 +6,40 @@ system_type=$(uname -s)
 # Check if we are root
 sudo -v
 # Make sure we stay sudo during the script
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do
+    sudo -n true
+    sleep 60
+    kill -0 "$$" || exit
+done 2>/dev/null &
 
 # Install stuff when on mac
 if [ "$system_type" = "Darwin" ]; then
     xcode-select --install
+
+    # Check if Homebrew is installed, if not install Homebrew
+    if ! command -v brew &>/dev/null; then
+        echo "Homebrew could not be found, installing Homebrew"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        # On linux we need extra steps
+        BREW_PREFIX=$(brew --prefix)
+        if [ "$system_type" = "Linux" ]; then
+            eval "$("$BREW_PREFIX/bin/brew" shellenv)"
+        fi
+    else
+        echo "Homebrew is installed, updating Homebrew"
+        brew update
+    fi
+
+    brew install yadm
 fi
+
 # Install stuff when on linux
 if [ "$system_type" = "Linux" ]; then
     sudo apt-get update
-    sudo apt-get install -y build-essential
+    sudo apt-get install -y build-essential yadm
 fi
 
+# Clone the dotfiles repo
+cd "$HOME" || exit
 
-# Check if Homebrew is installed, if not install Homebrew
-if ! command -v brew &> /dev/null
-then
-    echo "Homebrew could not be found, installing Homebrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    # On linux we need extra steps
-    BREW_PREFIX=$(brew --prefix)
-    if [ "$system_type" = "Linux" ]; then
-        eval "$("$BREW_PREFIX/bin/brew" shellenv)"
-    fi
-else
-    echo "Homebrew is installed, updating Homebrew"
-    brew update
-fi
-
-# Clone dotfiles repo
-git clone https://github.com/BobcatProgrammer/dotfiles.git ~/.dotfiles
-cd ~/.dotfiles || exit
-
-# Symlink .config to ~/.config
-ln -s ~/.dotfiles/.config ~/.config
-# symlink bin to ~/.bin
-ln -s ~/.dotfiles/bin ~/.bin
-
-
-# Install Brewfile
-brew bundle --file=.config/install/Brewfile
-brew cleanup
-
-# When on mac install macos defaults
-if [ "$system_type" = "Darwin" ]; then
-    source .config/install/macos.sh
-fi
+yadm clone https://github.com/BobcatProgrammer/dotfiles.git
