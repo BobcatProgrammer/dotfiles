a8a588141ff19ce526c3308e1228336a7b389058
Simon Trockel <mail@simon-trockel.de>
2023-08-08 19:36:25 +0000

    feat: :wrench: Update Fish Config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,34 @@
-# add custom executables (e.g. git-shalector) to path
-set --prepend PATH "$HOME/bin"
+
+# ███████╗██╗███████╗██╗  ██╗
+# ██╔════╝██║██╔════╝██║  ██║
+# █████╗  ██║███████╗███████║
+# ██╔══╝  ██║╚════██║██╔══██║
+# ██║     ██║███████║██║  ██║
+# ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
+# A smart and user-friendly command line
+# https://fishshell.com
+
+#######################################################################
+#                            main settings                            #
+#######################################################################
+
+fish_add_path /usr/local/sbin
+fish_add_path $HOME/bin
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
+fish_add_path /opt/homebrew/opt/ruby/bin
+
+# set default username to hide user@host ... see agnoster theme
+set DEFAULT_USER trockels
+
+
+# theme
+set theme_color_scheme "nord"
+
+
+# disable fish greeting
+set fish_greeting
+
+# -> Starting in kubectl v1.25, this plugin will be required for authentication.
+# -> https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
