cb419a6c610c412f52f1ec309f3a78542b0be93d
Simon Trockel <mail@simon-trockel.de>
2024-02-13 20:45:54 +0100

    Update tmux config, change alacritty theme, add fish plugins

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,10 +21,14 @@ fish_add_path /opt/homebrew/opt/ruby/bin
 # set default username to hide user@host ... see agnoster theme
 set DEFAULT_USER trockels
 
+# Set tmux config path
+set -Ux fish_tmux_config $HOME/.config/tmux/tmux.conf
 
 # theme
-set theme_color_scheme "nord"
+set theme_color_scheme "catppuccin"
 
+# Set icons as default for exa
+set -Ux EXA_STANDARD_OPTIONS --icons
 
 # disable fish greeting
 set fish_greeting
@@ -32,3 +36,7 @@ set fish_greeting
 # -> Starting in kubectl v1.25, this plugin will be required for authentication.
 # -> https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+
+# Set k9s dir to XDG_CONFIG_HOME
+export K9S_CONFIG_DIR=$HOME/.config/k9s
+
--- .config/fish/functions/ls.fish
+++
@@ -1,8 +0,0 @@
-# #!/usr/bin/env fish
-
-# function ls --description 'ls with improved defaults'
-#     # -G Enable colorized output
-#     # -T When used with the -l option, display complete time information for the file
-#     # -h When used with the -l option, use unit suffixes: Byte, Kilobyte etc.
-#     command ls -GTh $argv
-# end
